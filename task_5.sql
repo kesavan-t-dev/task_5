419ddbd520014a1c902e14d5b70f7d403a152d48
Kesavan T <kesavan.t@mitrahsoft.in>
2026-01-01 20:58:16 +0530

    Refactor SQL script to focus on temp tables and variables
    Removed project and task table creation and insertion scripts. Added temporary and table variable declarations with sample data insertion for medium and high priority tasks.

--- task_5.sql
+++ task_5.sql
@@ -1,64 +1,6 @@
 --USE DATABASE
 use kesavan_db
 GO
---Project Table
-CREATE TABLE project
-(
-	project_id INT IDENTITY(1,1) PRIMARY KEY,
-	project_name VARCHAR(150) UNIQUE NOT NULL,
-	starts_date DATE NOT NULL,
-	end_date DATE NOT NULL,
-	budget MONEY ,
-	statuss VARCHAR(50) DEFAULT 'Not Started',
-
-	--Constraints for end date field
-	    CONSTRAINT CHECK_end_date_After_starts_date 
-        CHECK (end_date >= starts_date),
-)
-GO
-
---Inserting values:
-INSERT INTO project (project_name, starts_date, end_date, budget, statuss)
-VALUES 
-    ('Website Redesign', '2025-01-01', '2025-06-30', 15000.00, 'In Progress'),
-    ('Mobile App Development', '2025-02-15', '2025-07-15', 25000.00, 'Not Started'),
-    ('Market Research', '2025-03-01', '2025-05-31', 10000.00, 'Completed'),
-    ('Annual Report Preparation', '2025-04-01', '2025-12-31', 12000.00, 'In Progress')
-GO
-
---task table creation
-CREATE TABLE task(
-	task_id INT IDENTITY(1,1) PRIMARY KEY,
-	task_name VARCHAR(150) NOT NULL,
-	descriptions VARCHAR(255) NOT NULL,
-	starts_date DATE NOT NULL,
-	due_date DATE NOT NULL,
-		--Constraints for end date field
-	    CONSTRAINT CHECK_due_date_After_starts_date 
-          CHECK (due_date >= starts_date),
-	prioritys VARCHAR(150) 
-		CONSTRAINT CK_Task_Priority CHECK (prioritys IN ('Low', 'Medium', 'High')),
-	statuss VARCHAR(70) DEFAULT 'Pending',
-	project_id INT FOREIGN KEY REFERENCES project(project_id)
-);
-GO
-
---inserting task values
-INSERT INTO task (task_name, descriptions, starts_date, due_date, prioritys, statuss, project_id)
-VALUES 
-    ('Initial Design', 'Design phase for the new website', '2025-01-02', '2025-02-28', 'High', 'Completed', 1),
-    ('UI Development', 'Development of user interface components', '2025-03-01', '2025-05-15', 'Medium', 'In Progress', 1),
-    ('Quality Assurance', 'Testing and quality assurance', '2025-05-16', '2025-06-15', 'High', 'Pending', 1),
-    ('API Development', 'Developing APIs for the mobile app', '2025-02-16', '2025-04-30', 'Medium', 'Completed', 2),
-    ('Beta Testing', 'Conducting beta testing for the mobile app', '2025-05-01', '2025-06-30', 'High', 'In Progress', 2),
-    ('Survey Analysis', 'Analyzing market research surveys', '2025-03-02', '2025-04-15', 'Low', 'Completed', 3),
-    ('Report Drafting', 'Drafting the final report based on research', '2025-04-16', '2025-05-30', 'Medium', 'Pending', 3),
-    ('Financial Statements', 'Preparing financial statements for the annual report', '2025-04-02', '2025-07-15', 'High', 'In Progress', 4),
-    ('Final Review', 'Final review and submission of the annual report', '2025-07-16', '2025-12-15', 'High', 'Pending', 4),
-    ('Client Feedback Incorporation', 'Incorporating feedback from the client into the project', '2025-02-01', '2025-03-15', 'Medium', 'In Progress', 1),
-    ('Launch Preparation', 'Preparing for the official launch of the mobile app', '2025-06-01', '2025-07-01', 'High', 'Pending', 2);
-    
-
 
 --- 1. Temporary Table
    
@@ -126,4 +68,51 @@ CREATE TABLE ##GlobalTempTable
     Budget DECIMAL(18,2),
     Priority NVARCHAR(20)
 );
+/*
+Insert Sample Data
+(Insert records from **task table** where **priority = 'Medium'**)
+*/
+
+INSERT INTO ##GlobalTempTable (project_name, budget, prioritys)
+SELECT
+    p.project_name,
+    p.budget,
+    t.prioritys
+FROM
+    task t
+INNERJOIN
+    project p
+ON
+    t.project_id= p.project_id
+WHERE
+    t.prioritys='Medium';
+--Display the table.
+SELECT*
+FROM ##GlobalTempTable;
+
+--Declare table variable 
+DECLARE@TableVariableTABLE
+(
+    task_id INT IDENTITY(1,1),
+    task_name VARCHAR(100),
+    due_dates DATE,
+    priority NVARCHAR(20)
+);
+
+--insert data
+INSERT INTO@TableVariable (task_name, due_dates, prioritys)
+SELECT
+    task_name,
+    due_date,
+    prioritys
+FROM
+    task
+WHERE
+    prioritys='High';
+
+
+--Display table
+SELECT*
+FROM@TableVariable;
+
 
